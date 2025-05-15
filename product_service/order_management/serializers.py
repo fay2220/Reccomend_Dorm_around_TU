319a122f1c6a1215e97a2709d1a23a6563161b73
Theerapat <168794803+fay2220@users.noreply.github.com>
2025-05-15 12:23:56 +0700

    Update serializers.py

--- product_service/order_management/serializers.py
+++ product_service/order_management/serializers.py
@@ -1,33 +1,24 @@
 from rest_framework import serializers
 from .models import InterestRequest
-from product_management.models import Dorm
+from product_management.models import Dorm  # สำหรับดึงชื่อ dorm
 
 class InterestRequestSerializer(serializers.ModelSerializer):
-    dorm_name = serializers.CharField(source='dorm.name', read_only=True)
+    dorm_name = serializers.SerializerMethodField(read_only=True)
 
     class Meta:
         model = InterestRequest
         fields = '__all__'
-        read_only_fields = ['user', 'dorm'] 
+
+    def get_dorm_name(self, obj):
+        try:
+            dorm = Dorm.objects.get(id=obj.dorm_id)
+            return dorm.name
+        except Dorm.DoesNotExist:
+            return None
 
     def validate(self, data):
-        required_fields = ['username', 'email', 'tel', 'address', 'zone']
+        required_fields = ['username', 'email', 'tel', 'address', 'zone', 'dorm_id']
         for field in required_fields:
             if not data.get(field):
                 raise serializers.ValidationError({field: "This field is required."})
         return data
-
-    def create(self, validated_data):
-        request = self.context.get('request')
-        dorm_id = self.initial_data.get('dorm_id')
-
-        try:
-            dorm = Dorm.objects.get(pk=dorm_id)
-        except Dorm.DoesNotExist:
-            raise serializers.ValidationError({'dorm_id': 'ไม่พบหอพักที่เลือก'})
-
-        return InterestRequest.objects.create(
-            user=request.user,
-            dorm=dorm,
-            **validated_data
-        )
